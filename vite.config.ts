69f9754adb2c5466b2f7657bc359c5803374fc2c
akira1552 <pektyer305@gmail.com>
2025-06-14 11:35:00 +0900

    upgrade to React Router v7

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,10 @@
-import {reactRouter}
+import {reactRouter} from "@react-router/dev/vite"
+import { defineConfig } from "vite"
+import tsconfigPaths from "vite-tsconfig-paths"
+
+export default defineConfig({
+    plugins: [
+        reactRouter(),
+        tsconfigPaths(),
+    ],
+});
