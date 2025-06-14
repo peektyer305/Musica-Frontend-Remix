2e05cd0803653ec9eadb6b37ad7d3625d1cdeb96
akira1552 <pektyer305@gmail.com>
2025-06-14 11:17:27 +0900

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,1 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-export default defineConfig({
-  plugins: [
-    reactRouter({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
-});
+import {reactRouter}
